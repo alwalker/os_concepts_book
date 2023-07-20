bbad658c8881950eb3ee6b13affc3274c4656b3d
alwalker21@gmail.com <alwalker21@gmail.com>
2023-07-20 08:50:18 -0500

    updated dev container stuff

--- code/ch-02/copy_file_exercise/c.c
+++ code/ch-02/copy_file_exercise/c.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-   FILE *in_file = fopen("source.txt","r");;
+   FILE *in_file = fopen("source.txt", "r");
    char *contents = (char *)calloc(2048, sizeof(char));
-    fgets(contents, 2048, in_file);
+   fgets(contents, 2048, in_file);
    fclose(in_file);
 
-   FILE *out_file = out_file = fopen("c_out.txt","w");
-   fprintf(out_file,"%s",contents);
+   FILE *out_file = out_file = fopen("c_out.txt", "w");
+   fprintf(out_file, "%s", contents);
    fclose(out_file);
 
    return 0;
